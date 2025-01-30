6195ef88fa37112f77f2be81e726b7719fcb550d
Sanghun Lee <lee@sanghun.io>
2025-01-30 21:26:59 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CaretAudioProcessing",
-            url: "https://github.com/dsa28s/webrtc-audio-processing/releases/download/1.1.7/CaretAudioProcessing.xcframework@1.1.7.zip",
-            checksum: "a41ba30aaddbcad2f627a473cedff80e1d7c2df4d98b78389cdae1f9a21ebf9a"
+            url: "https://github.com/dsa28s/webrtc-audio-processing/releases/download/1.1.11/CaretAudioProcessing.xcframework@1.1.11.zip",
+            checksum: "0d968a5ed1348960e0e147ab16ddebea53019ac1891d32b61a07726e1b06a87e"
         ),
         .target(
             name: "CaretAudioProcessingSwift",
